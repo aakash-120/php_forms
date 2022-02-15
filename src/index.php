693ae1cd7197c3ffa74383f8ce619ade12f8f772
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-02-15 16:59:43 +0530

    Write a program to calculate area

--- src/index.php
+++ src/index.php
@@ -1,64 +1,54 @@
 <?php
 
-if (isset($_POST['SUBMIT'])) {
-    $units = $_POST['units'];
-    if (!empty($units)) {
-        $answer = bill_amount($units);
-        echo "$answer";
-        
-    }
-}
-function bill_amount($units)
-{
-    global $bill_price ;
+$n1 = $_GET ["length"];  
+if (!preg_match ("/^[0-9]*$/", $n1) ){  
+    $ErrMsg = "Only numeric value is allowed.";  
+    echo $ErrMsg;  
+} else {  
+    echo $mobileno;  
+}  
+$n2 = $_GET ["width"];  
+if (!preg_match ("/^[0-9]*$/", $n2) ){  
+    $ErrMsg = "Only numeric value is allowed.";  
+    echo $ErrMsg;  
+} else {  
+    echo $mobileno;  
+} 
 
-    if($units <= 50)
-    {
-        $bill_price = $units * 3.50;   
-    }
-    else if($units > 50 && $units <= 150)
-    {
-        $fifty_bill = 50 * 3.50;
-        $units = $units - 50;
-        $bill_price = $fifty_bill + $units * 4.00;
 
-    }
-    else if($units > 150 && $units <= 250)
-    {
-        $fifty_bill = 50 * 3.50;
-        $hundred_bill = 100 * 4.00;
-        $units = $units - 150;
-        $bill_price = $fifty_bill + $hundred_bill + $units * 5.20;
-        
-    }
-    else
-    {
-        $fifty_bill = 50 * 3.50;
-        $hundred_bill = 100 * 4.00;
-        $two_hundred_bill= 100 * 5.20;
-        $units = $units - 250;
-        $bill_price = $fifty_bill + $hundred_bill +$two_hundred_bill +$units * 6.50;
-    }
-    return $bill_price;
-}
+
+$num1 = (float)$_GET["length"];
+$num2 = (float)$_GET["width"];
+
+$per = 2 * ($num1 + $num2);
+$area = $num1 * $num2;
+
+echo "$per<br>";
+echo "$area";
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-<form action="" method="post">
-Enter units to calculate bill: <input type="text" name="units"><br>
-<input type="submit" name = SUBMIT>
+    
+<form action="" method="get">
+Length of Rectangle: <input type="text" name="length"><br>
+Width of Rectangle: <input type="text" name="width"><br>
+<input type = "submit" name = "SUBMIT" id = "id1" value = "Calculate area & Perimeter">
 </form>
 
+<div>
+<p> Area is <?php echo $area?> sq. mtr</p>
+<p> Perimeter is <?php echo $per?> mtr</p>
 
-
+</div>
 </body>
 </html>
