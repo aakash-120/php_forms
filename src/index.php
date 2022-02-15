a0168698c7fa52f9bfc653f6e89dcaefa3f08a73
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-02-15 19:33:22 +0530

    Write a program to upload image

--- src/index.php
+++ src/index.php
@@ -1,76 +1,37 @@
 <?php
-
-function conversion()
-{
-    $n1 = $_GET ["input"];  
-    if (!preg_match ("/^[0-9]*$/", $n1) )
-    {  
-        $ErrMsg = "Only numeric value is allowed.";  
-        echo $ErrMsg;  
-    }  
-    else
-    {
-        $num= "";
-        $num1 = (float)$_GET["input"];
-        $num2 = (float)$_GET["radiobtn"];
-
-        if ($num2 ==1) 
-        {
-            $num = $num1 * 60;
-        } 
-        else 
-        {
-            $num = $num1 * 60 * 60;
-        }
-    }
-    return $num;
-}
-
-
-if(array_key_exists('SUBMIT', $_GET)) {
-    $n = conversion();
-}
-
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $file_type=$_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $extensions= array("png");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"uploads/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<div id = "id1">
-<form action="" method="get">
-<input type="text" name="input"><br>
-
-<input type = "radio" name = "radiobtn" value=1>hours to min
-<br>
-<input type = "radio" name = "radiobtn" value=0>hours to seconds
-<br>
-<p><?php 
-if(array_key_exists('input', $_GET))
-{
-    if($_GET["radiobtn"]==1 )
-    {
-        echo $_GET['input']." hours to $n min";
-    }
-    else
-    {
-        echo $_GET['input']." hours to $n seconds";
-    }
-}
-?></p>
-
-
-<input type = "submit" name = "SUBMIT" id = "id2" value = "Convert">
-</form>
-</div>
-
-</body>
+<html>
+   <body>
+      
+      <form action="" method="POST" enctype="multipart/form-data">
+         <input type="file" name="image" />
+         <input type="submit"/>
+      </form>
+      
+   </body>
 </html>
